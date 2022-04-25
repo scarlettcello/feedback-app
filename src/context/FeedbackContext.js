dc2271b04cc573e3220b5a4a781d0d125f45a1f4
scarlettcello <scarlettcello@gmail.com>
2022-04-25 02:09:17 -0400

    initial commit

---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,61 @@
+import {v4 as uuidv4} from 'uuid';
+import { createContext, useState } from 'react';
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: 'This is Feedback item one',
+      rating: 10
+    },
+    {
+      id: 2,
+      text: 'This is Feedback item two',
+      rating: 7
+    },
+    {
+      id: 3,
+      text: 'This is Feedback item three',
+      rating: 4
+    }
+  ]);
+
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false
+  })
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4;
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  const updateFeedback = (id, updItem) => {
+    setFeedback(feedback.map((item) => item.id === id ? { ... item, ...updItem} : item));
+  }
+
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true
+    })
+  }
+
+  return (
+    <FeedbackContext.Provider 
+      value={{ feedback, feedbackEdit, addFeedback, deleteFeedback, editFeedback, updateFeedback }}
+    >
+      { children}
+    </FeedbackContext.Provider>
+  );
+}
+
+export default FeedbackContext;
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'There are many variations of passages of Lorem Ipsum available'
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet..'
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.'
+  }
+]
+
+export default FeedbackData;
